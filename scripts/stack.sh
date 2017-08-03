7c4acc0db7ae1acc0892e2cd7a447db5fcd56b8b
Andreas Schenk <andreas.schenk@fmi.ch>
2017-08-03 15:24:12 +0200

    optimization for stack.sh

--- scripts/stack.sh
+++ scripts/stack.sh
@@ -5,6 +5,7 @@ mkdir -p $destination_path/movies_raw $destination_path/micrographs_raw $destina
 
 pixel_size=`calculate "1e10*$apix_x"`
 stack_log=$destination_path/movies_raw/${short_name}_stack.log
+ice_ratio_log=$destination_path/movies_raw/${short_name}_ice_ratio.log
 raw_stack=$destination_path/movies_raw/${short_name}.mrcs
 raw_average_thumbnail=$destination_path/micrographs_raw/${short_name}.png
 raw_average=$destination_path/micrographs_raw/${short_name}.mrc
@@ -19,13 +20,17 @@ if [ ! -e $raw_stack ] || [ ! -e $raw_average_thumbnail ]; then
   e2proc2d.py  --average $raw_stack $raw_average>> $stack_log
   e2proc2d.py  --fouriershrink 7.49609375  $raw_average $raw_average_thumbnail>> $stack_log
 fi
+if [ ! -e $ice_ratio_log ] ; then
+  $STACK_GUI_SCRIPTS/ice_ratio.py $raw_average $pixel_size 5.0 3.7 0.4> $ice_ratio_log
+fi
 
-ice_ratio=`$STACK_GUI_SCRIPTS/ice_ratio.py $raw_average $pixel_size 5.0 3.7 0.4`
+ice_ratio=`tail -n 1 $ice_ratio_log`
+
 if (( $(echo "$ice_ratio > 1.5" |bc -l) )); then
   export="false"
 else
   export="true"
 fi
 RESULTS raw_average_thumbnail raw_stack ice_ratio
-FILES raw_stack stack_log raw_average raw_average_thumbnail xml
+FILES raw_stack stack_log raw_average raw_average_thumbnail xml ice_ratio_log
 
