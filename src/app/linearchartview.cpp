c26d892aacf04605b96d06579ebc5e9a2dc25bb0
Andreas <andreas.schenk@fmi.ch>
2019-11-24 13:40:51 +0100

    updated license headers

--- src/app/linearchartview.cpp
+++ src/app/linearchartview.cpp
@@ -1,3 +1,24 @@
+//------------------------------------------------------------------------------
+//
+// Author: Andreas Schenk
+// Friedrich Miescher Institute, Basel, Switzerland
+//
+// This file is part of CryoFLARE
+//
+// Copyright (C) 2019 by the CryoFLARE Authors
+//
+// This program is free software; you can redistribute it and/or modify it under
+// the terms of the GNU General Public License as published by the Free
+// Software Foundation; either version 3.0 of the License.
+// This program is distributed in the hope that it will be useful, but WITHOUT
+// ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+// FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+// details.
+//
+// You should have received a copy of the GNU General Public License
+// along with CryoFLARE.  If not, see <http://www.gnu.org/licenses/>.
+//
+//------------------------------------------------------------------------------
 #include "imagetablemodel.h"
 #include "linearchartview.h"
 
